68934451cb26e23d5e4de8e23dacbb925a22903f
MihaelGolob <48100773+MihaelGolob@users.noreply.github.com>
2022-08-31 08:14:32 +0200

    Fixed folder structure

---
+++ Editor/ObjectFollowerEditor.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+using UnityEditor;
+
+/// <summary>
+/// This class defines a new more simple inspector for the ObjectFollow.cs script.
+/// </summary>
+[CustomEditor(typeof(ObjectFollower))]
+public class ObjectFollowerEditor : Editor {
+    public override void OnInspectorGUI() {
+        var script = target as ObjectFollower;
+
+        // OBJECT TO FOLLOW
+        EditorGUILayout.LabelField("The object to follow", EditorStyles.boldLabel);
+        EditorGUILayout.PropertyField(serializedObject.FindProperty("_target"));
+        
+        // FOLLOW POSITION
+        var followPosition = serializedObject.FindProperty("_followPosition");
+        EditorGUILayout.PropertyField(followPosition);
+
+        if (followPosition.boolValue) {
+            EditorGUI.indentLevel++;
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("_positionOffset"));
+
+            var lerp = serializedObject.FindProperty("_lerpMovement");
+            EditorGUILayout.PropertyField(lerp);
+            if (lerp.boolValue) {
+                EditorGUI.indentLevel++;
+                EditorGUILayout.PropertyField(serializedObject.FindProperty("_lerpMovementSpeed"),
+                    new GUIContent("Lerp movement", "smooth movement"));
+                EditorGUI.indentLevel--;
+            }
+            EditorGUI.indentLevel--;
+        }
+        
+        // FOLLOW ROTATION
+        var followRotation = serializedObject.FindProperty("_followRotationMode");
+        EditorGUILayout.PropertyField(followRotation);
+        
+        if (followRotation.intValue == 2) {
+            EditorGUI.indentLevel++;
+            var lerp = serializedObject.FindProperty("_lerpRotation");
+            EditorGUILayout.PropertyField(lerp);
+            if (lerp.boolValue) {
+                EditorGUI.indentLevel++;
+                EditorGUILayout.PropertyField(serializedObject.FindProperty("_lerpRotationSpeed"),
+                    new GUIContent("Lerp movement", "smooth movement"));
+                EditorGUI.indentLevel--;
+            }
+            EditorGUI.indentLevel--;
+        }
+        
+        // FOLLOW SCALE
+        EditorGUILayout.PropertyField(serializedObject.FindProperty("_followScale"));
+
+        //update the serialized fields
+        serializedObject.ApplyModifiedProperties();
+    }
+}
